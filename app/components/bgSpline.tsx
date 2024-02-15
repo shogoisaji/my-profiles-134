41394705afc2d0680f7a30d526d1044044df7612
shogoisaji <simacsimac9@gmail.com>
2024-02-15 19:16:20 +0900

    ✨ Add alternative background image for mobile devices

--- app/components/bgSpline.tsx
+++ app/components/bgSpline.tsx
@@ -1,15 +1,26 @@
 'use client'
 
 import Spline from '@splinetool/react-spline'
+import Image from 'next/image'
 
 export const BgSpline = () => {
     return (
         //スマホではSplineがクラッシュするので暫定対策
-        <div className="md:block hidden">
-            <Spline
-                className="fixed top-0 left-0"
-                scene="https://prod.spline.design/lSSNw-vp-4c7z7wz/scene.splinecode"
-            />
+        <div>
+            <div className="lg:block hidden">
+                <Spline
+                    className="fixed top-0 left-0"
+                    scene="https://prod.spline.design/lSSNw-vp-4c7z7wz/scene.splinecode"
+                />
+            </div>
+            <div className="lg:hidden block fixed top-0 left-0">
+                <Image
+                    src="/images/alt_bg.png"
+                    width={1920}
+                    height={1080}
+                    alt="background"
+                />
+            </div>
         </div>
     )
 }
