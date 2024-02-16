e6d1bd5043bb9654551598c48e4c27a8ea477da4
shogoisaji <simacsimac9@gmail.com>
2024-02-16 10:11:19 +0900

    ♻️ Refactor rive detail button mobile

--- app/components/detailButton.tsx
+++ app/components/detailButton.tsx
@@ -1,10 +1,24 @@
 import Rive from '@rive-app/react-canvas'
 import { useWindowSize } from '@/app/customHooks/useWindowSize'
+import { useEffect, useState } from 'react'
 
 export const DetailButton = () => {
     const [width, height] = useWindowSize()
-    const riveSource =
-        width > 768 ? 'rive/detail.riv' : 'rive/detail_auto_hover.riv'
-    console.log(riveSource)
-    return <Rive key={riveSource} src={riveSource} stateMachines="state" />
+    const [isOverMd, setIsOverMd] = useState(false)
+    useEffect(() => {
+        setIsOverMd(width > 768)
+    }, [width])
+
+    const riveSource = isOverMd
+        ? 'rive/detail.riv'
+        : 'rive/detail_auto_hover.riv'
+    return (
+        <Rive
+            key={riveSource}
+            src={riveSource}
+            stateMachines="state"
+            onClick={() => console.log('clicked')}
+            shouldDisableRiveListeners={isOverMd ? false : true}
+        />
+    )
 }
